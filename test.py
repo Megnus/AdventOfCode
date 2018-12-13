2e3bc144249986d1d0e46b0bc02b3e23f0930eab
ARNA-00982-L <magnus.sundstrom@flir.se>
2018-12-13 13:49:36 +0100

    Working on puzzle 8 part 2.

--- test.py
+++ test.py
@@ -3,144 +3,39 @@ import re
 from functools import reduce
 
 
-def contains(array, val):
-    try:
-        return array.index(val) >= 0
-    except ValueError:
-        return False
+def func(v, n):
+    global s
+    for i in range(0, n):
+        c = v[0]
+        e = v[1]
+        v = v[2:]
+        v = func(v, c)
+        m = v[:e]
+        v = v[e:]
+        s += m
+    return v
 
-# GKPTSLUXBIJMNCADFOVHEWYQRZ
-f = open('Input/input_day_7.txt', 'r')
+
+def calc(v, n):
+    global s
+    for i in range(0, n):
+        c = v[0]
+        e = v[1]
+        v = v[2:]
+        v, s_ = calc(v, c)
+        m = v[:e]
+        v = v[e:]
+        s += m
+    return v
+
+
+f = open('Input/input_day_8.txt', 'r')
 data = f.read()
 f.close()
+ar = list(map(lambda n: int(n), data.split(' ')))
+s = []
 
-ar = data.splitlines()
-ar = [[re.findall('(?<=Step\s)[A-Z]', x)[0], re.findall('(?<=step\s)[A-Z]', x)[0]] for x in ar]
+print('Raw:', ar)
+func(ar, 1)
+print('Sum:', sum(s))
 
-stepArray = []
-
-while len(ar) > 0:
-      temp = list(set(map(lambda x: x[1], ar)))
-      temp = list(filter(lambda x: not contains(temp, x[0]), ar))
-      step = sorted(set(list(map(lambda x: x[0], temp))))
-      stepArray += step[0]
-      print(stepArray)
-      temp = list(filter(lambda x: not contains(stepArray, x[0]), ar))
-      ar = sorted(temp, key=lambda x: x[0])
-      print(ar)
-
-print(''.join(stepArray))
-print(''.join(stepArray) == 'GKPTSLUXBIJMNCADFOVHEWYQRZ')
-exit()
-
-# Get right elements from basic list
-temp = list(set(map(lambda x: x[1], ar)))
-
-# Get all left element that is not in right element.
-temp = list(filter(lambda x: not contains(temp, x[0]), ar))
-
-# Get all al left elements that is unique sorted.
-step = sorted(set(list(map(lambda x: x[0], temp))))
-
-stepArray += step[0]
-
-print(stepArray)
-
-temp = list(filter(lambda x: not contains(stepArray, x[0]), ar))
-ar = sorted(temp, key=lambda x: x[0])
-print(ar)
-
-# Get right elements from basic list
-temp = list(set(map(lambda x: x[1], ar)))
-
-# Get all left element that is not in right element.
-temp = list(filter(lambda x: not contains(temp, x[0]), ar))
-
-# Get all al left elements that is unique sorted.
-step = sorted(set(list(map(lambda x: x[0], temp))))
-
-stepArray += step[0]
-print(stepArray)
-
-#-----------------------
-
-temp = list(filter(lambda x: not contains(stepArray, x[0]), ar))
-ar = sorted(temp, key=lambda x: x[0])
-print(ar)
-
-# Get right elements from basic list
-temp = list(set(map(lambda x: x[1], ar)))
-
-# Get all left element that is not in right element.
-temp = list(filter(lambda x: not contains(temp, x[0]), ar))
-
-# Get all al left elements that is unique sorted.
-step = sorted(set(list(map(lambda x: x[0], temp))))
-print(step)
-stepArray += step[0]
-print(stepArray)
-
-#-----------------------
-
-temp = list(filter(lambda x: not contains(stepArray, x[0]), ar))
-ar = sorted(temp, key=lambda x: x[0])
-print(ar)
-
-# Get right elements from basic list
-temp = list(set(map(lambda x: x[1], ar)))
-
-# Get all left element that is not in right element.
-temp = list(filter(lambda x: not contains(temp, x[0]), ar))
-
-# Get all al left elements that is unique sorted.
-step = sorted(set(list(map(lambda x: x[0], temp))))
-print(step)
-stepArray += step[0]
-print(stepArray)
-
-#-----------------------
-
-temp = list(filter(lambda x: not contains(stepArray, x[0]), ar))
-ar = sorted(temp, key=lambda x: x[0])
-print(ar)
-
-# Get right elements from basic list
-temp = list(set(map(lambda x: x[1], ar)))
-
-# Get all left element that is not in right element.
-temp = list(filter(lambda x: not contains(temp, x[0]), ar))
-
-# Get all al left elements that is unique sorted.
-step = sorted(set(list(map(lambda x: x[0], temp))))
-print(step)
-stepArray += step[0]
-print(stepArray)
-
-
-
-
-
-
-
-
-
-
-
-
-exit()
-temp1 = list(set(map(lambda x: x[1], ar1)))
-
-# Get all left element that is not in right element.
-temp1 = list(filter(lambda x: not contains(temp1, x[0]), ar1))
-
-# Get all al left elements that is unique sorted.
-step2 = sorted(set(list(map(lambda x: x[0], temp1))))
-
-print(step2)
-print(temp1)
-
-
-#temp = sorted(list(set(map(lambda x: x[1], temp))))
-#temp = sorted(temp, key=lambda x: x[0])
-
-#print(temp)
