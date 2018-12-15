017b52f25b2ab01e48254a65e63b0836554a49ce
Magnus Sundström <megnus@hotmail.com>
2018-12-15 18:59:32 +0100

    Finished both puzzel 8 parts.

--- Day_8.py
+++ Day_8.py
@@ -3,63 +3,38 @@ import re
 from functools import reduce
 
 
-class Node:
-    def __init__(self, array_data):
-        print(array_data)
-        self.array_data = array_data
-        self.child_nodes_entries = array_data[0]
-        self.metadata_entries = array_data[1]
-        if self.child_nodes_entries == 0:
-            self.metadata = array_data[2:2 + self.metadata_entries]
-            self.array_data = self.array_data[2 + self.metadata_entries:]
-            return
-
-        self.node = Node(self.array_data[2:])
-        self.array_data = self.node.array_data
+def func(v, n):
+    global s
+    for i in range(0, n):
+        c = v[0]
+        e = v[1]
+        v = v[2:]
+        v = func(v, c)
+        m = v[:e]
+        v = v[e:]
+        s += m
+    return v
 
 
-def func(ar, s):
-    #while len(ar) > 0:
-        nc = ar[0]
-        me = ar[1]
-        #ar = ar[2:]
-        print([nc, me], ar[2:])
-
-        if nc == 0 or len(ar) == me:
-            ar = ar[2:]
-            print(':', ar, ar[:me])
-            return ar, s + sum(ar[:me])
-        ar, s = func(ar[2:], s)
-
+def calc(v, n):
+    su = []
+    for i in range(0, n):
+        c = v[0]
+        e = v[1]
+        v = v[2:]
+        v, su_ = calc(v, c)
+        m = v[:e]
+        v = v[e:]
+        print('New shit:',  su_, m)
+        su += m + su_
+    return v, su
 
 
 f = open('Input/input_test.txt', 'r')
 data = f.read()
-f.close()
-
 ar = list(map(lambda n: int(n), data.split(' ')))
-#ar, s = func(ar, 0)
-print(func(ar, 0))
-exit()
-
-s = 0
-nc = ar[0]
-me = ar[1]
-me_ar = ar[len(ar) - me:]
-s = sum(me_ar)
-ar = ar[2:len(ar) - me]
-print([nc, me], ar, me_ar)
-
-
-while len(ar) > 0:
-    print(ar)
-    nc = ar[0]
-    me = ar[1]
-    ar = ar[2:]
-    if nc == 0:
-        me_ar = ar[:me]
-        ar = ar[me:]
-        s += sum(me_ar)
-        print([nc, me], ar, me_ar)
-
-print(s)
+f.close()
+s = []
+print('Raw:', ar)
+print(calc(ar, 1))
+print('Sum:', sum(s))
