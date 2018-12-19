e27389f770c47ca0ea859f7780bdac719c4f6071
ARNA-00982-L <magnus.sundstrom@flir.se>
2018-12-19 10:35:06 +0100

    Puzzle 13 finished.

--- Day_13.py
+++ Day_13.py
@@ -10,6 +10,7 @@ def is_in(v, e):
     except ValueError:
         return False
 
+
 def find(vec, elm):
     vec = list(map(lambda x: x[0], vec))
     try:
@@ -24,11 +25,12 @@ def get(vec, elm):
         return vec[idx][1]
 
 
-#car = [[x, y], [v, n]]
-def new_pos(car):
+# car = [[x, y], [v, n] is_moved]
+def new_pos(c):
     M = {'v': [0, 1], '<': [-1, 0], '>': [1, 0], '^': [0, -1]}
-    car[0][0] += M[car[1][0]][0]
-    car[0][1] += M[car[1][0]][1]
+    c[0][0] += M[c[1][0]][0]
+    c[0][1] += M[c[1][0]][1]
+    c[2] = True
     return car
 
 
@@ -39,13 +41,10 @@ def new_vel(car, track_p):
     N = [1, 0, -1]
 
     if track_p == '+':
-        xx = car[1][0]
-        ro = N[car[1][1]]
         index = S.index(car[1][0]) + N[car[1][1]]
         index = 3 if index < 0 else index % 4
         car[1][0] = S[index]
         car[1][1] = (car[1][1] + 1) % 3
-        #print(xx, car[1][0], ro)
     else:
       car[1][0] = M[track_p][car[1][0]]
 
@@ -53,8 +52,8 @@ def new_vel(car, track_p):
 
 
 f = open('Input/input_test.txt', 'r')
-f = open('Input/input_day_13_test.txt', 'r')
 f = open('Input/input_day_13.txt', 'r')
+
 data = f.read()
 f.close()
 data = data.splitlines()
@@ -67,88 +66,27 @@ for y in range(0, len(data)):
         if c == '\\' or c == '/' or c == '+':
             v.append([[x, y], c])
         elif c == '>' or c == '<' or c == '^' or c == 'v':
-            cars.append([[x, y], [c, 0], [0, 0]])
+            cars.append([[x, y], [c, 0], True])
 
-
-#nv = list(map(lambda x: x[0], v))
-#print(get(v, [7, 1]))
-
-count = 0
+collisions = []
 while True:
-    pos_array = []
-    idx = []
-    for car in cars:
-        #old_pos = [car[0][0], car[0][1]]
+    for car in sorted(cars):
+        if not car[2]:
+            continue
         new_pos(car)
-
         if list(map(lambda x: x[0], cars)).count(car[0]) > 1:
-            print(car[0])
-            for i in range(0, len(cars)):
-                if list(map(lambda o: o[0], cars)).count(cars[i][0]) > 1:
-                    idx.append(i)
-                idx.sort(key=lambda x: x, reverse=True)
-                for i in idx:
-                    cars.pop(i)
-
+            collisions.append(car[0])
+            cars = list(map())
+            for c in cars:
+                if c[0] == car[0]:
+                    c[2] = False
+            break
         track = get(v, car[0])
         if track:
             new_vel(car, track)
-    if len(idx) > 0:
-        idx.sort(key=lambda x: x, reverse=True)
-        for i in idx:
-            cars.pop(i)
-        print(idx, cars)
-
-
-
-
-    """
-    c_pos = list(map(lambda x: x[2], cars))
-    if len(list(filter(lambda x: c_pos.count(x[2]) > 1, cars))) > 0:
-        print(list(filter(lambda x: c_pos.count(x[2]) > 1, cars)))
-    le = len(cars)
-    cars = list(filter(lambda x: c_pos.count(x[2]) < 2, cars))
-    if le != len(cars):
-        print('2', le, len(cars))
-
-    c_pos = list(map(lambda x: x[0], cars))
-    if len(list(filter(lambda x: c_pos.count(x[0]) > 1, cars))) > 0:
-        print(list(filter(lambda x: c_pos.count(x[0]) > 1, cars)))
-    le = len(cars)
-    cars = list(filter(lambda x: c_pos.count(x[0]) < 2, cars))
-    if le != len(cars):
-        print('0', le, len(cars))
-    """
-    #print(len(cars))
+    cars = list(filter(lambda c: c[2], cars))
     if len(cars) == 1:
-        print(cars)
-        exit()
-    count += 1
+        break
 
-# 69,67
+print('First collision:', collisions, 'Last position:', cars[0][0])
 
-print(pos)
-
-"""
-123,18
-54,36
-122,69
-104,49
-24,102
-19,85
-32,107
-83,46
-71,123
-
-"""
-"""
-for y in range(0, len(data)):
-    x = 0
-    while True:
-        x = find(data[y], '\\', x)
-        if x >= 0:
-            print(x, y)
-            break
-"""
-
-#print(data[0].index('\\'))
