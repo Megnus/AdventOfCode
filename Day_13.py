77bbde55aa56e08af3635e7b73a52887ada617f5
ARNA-00982-L <magnus.sundstrom@flir.se>
2018-12-19 10:48:38 +0100

    Some minor refactoring of finished puzzle 13.

--- Day_13.py
+++ Day_13.py
@@ -26,29 +26,27 @@ def get(vec, elm):
 
 
 # car = [[x, y], [v, n] is_moved]
-def new_pos(c):
+def new_pos(cart):
     M = {'v': [0, 1], '<': [-1, 0], '>': [1, 0], '^': [0, -1]}
-    c[0][0] += M[c[1][0]][0]
-    c[0][1] += M[c[1][0]][1]
-    c[2] = True
-    return car
+    cart[0][0] += M[cart[1][0]][0]
+    cart[0][1] += M[cart[1][0]][1]
+    cart[2] = True
+    return cart
 
 
-def new_vel(car, track_p):
+def new_vel(cart, track_p):
     M = {'\\': {'v': '>', '<': '^', '>': 'v', '^': '<'},
          '/': {'v': '<', '<': 'v', '>': '^', '^': '>'}}
     S = ['>', '^', '<', 'v']
     N = [1, 0, -1]
-
     if track_p == '+':
-        index = S.index(car[1][0]) + N[car[1][1]]
+        index = S.index(cart[1][0]) + N[cart[1][1]]
         index = 3 if index < 0 else index % 4
-        car[1][0] = S[index]
-        car[1][1] = (car[1][1] + 1) % 3
+        cart[1][0] = S[index]
+        cart[1][1] = (cart[1][1] + 1) % 3
     else:
-      car[1][0] = M[track_p][car[1][0]]
-
-    return car
+      cart[1][0] = M[track_p][cart[1][0]]
+    return cart
 
 
 f = open('Input/input_test.txt', 'r')
@@ -59,34 +57,33 @@ f.close()
 data = data.splitlines()
 
 v = []
-cars = []
+carts = []
 for y in range(0, len(data)):
     for x in range(0, len(data[y])):
         c = data[y][x]
         if c == '\\' or c == '/' or c == '+':
             v.append([[x, y], c])
         elif c == '>' or c == '<' or c == '^' or c == 'v':
-            cars.append([[x, y], [c, 0], True])
+            carts.append([[x, y], [c, 0], True])
 
 collisions = []
 while True:
-    for car in sorted(cars):
-        if not car[2]:
+    for cart in sorted(carts):
+        if not cart[2]:
             continue
-        new_pos(car)
-        if list(map(lambda x: x[0], cars)).count(car[0]) > 1:
-            collisions.append(car[0])
-            cars = list(map())
-            for c in cars:
-                if c[0] == car[0]:
+        new_pos(cart)
+        if list(map(lambda c: c[0], carts)).count(cart[0]) > 1:
+            collisions.append(cart[0])
+            for c in carts:
+                if c[0] == cart[0]:
                     c[2] = False
             break
-        track = get(v, car[0])
+        track = get(v, cart[0])
         if track:
-            new_vel(car, track)
-    cars = list(filter(lambda c: c[2], cars))
-    if len(cars) == 1:
+            new_vel(cart, track)
+    carts = list(filter(lambda c: c[2], carts))
+    if len(carts) == 1:
         break
 
-print('First collision:', collisions, 'Last position:', cars[0][0])
+print('First collision:', collisions[0], 'Last position:', carts[0][0])
 
