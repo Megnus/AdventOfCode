b57c908b19ffdaac15f1cd69e32539e4d7dcbad8
ARNA-00982-L <magnus.sundstrom@flir.se>
2019-01-07 14:25:16 +0100

    Puzzle 15 finished and temporary files removed.

--- Day_15.py
+++ Day_15.py
@@ -255,7 +255,7 @@ def get_elf_survival_battle_score():
 
 def initialization():
     global p, w, h
-    p, w, h = initialization_input('Input/input_day_15_original.txt')
+    p, w, h = initialization_input('Input/input_day_15.txt')
 
 
 print('Part 1:', get_battle_score())
