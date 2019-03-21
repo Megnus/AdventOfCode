5c3df652ba71c6620a880d3ebd9b91539787d93f
ARNA-00982-L <magnus.sundstrom@flir.se>
2019-03-21 17:38:31 +0100

    Getting closer to solve puzzle 17 with some refactoring.

--- Day_17.py
+++ Day_17.py
@@ -28,41 +28,8 @@ def create_vector(data_xy):
     vec_y = [[[x, y[0]] for x in range(y[1], y[2])] for y in data_y]
     return [x for y in vec_x + vec_y for x in y]
 
-"""
-data = create_vector(initialization())
-print(data)
-max_col = max(data, key=lambda x: x[0])[0] + 2
-max_row = max(data, key=lambda x: x[1])[1] + 4
-print(max_col, max_row)
-matrix = [['.' for col in range(0, max_col)] for row in range(0, max_row)]
-for v in data:
-    col, row = v
-    matrix[row][col] = '#'
-    
-print(matrix)
-for i in range(0, max_row):
-    for j in range(494, max_col):
-        print(matrix[i][j], end='', flush=True)
-    print()
-"""
-
-'''
-target = [500, 0]
-x, y = target
-probe = matrix[y][x]
-# probe
-while probe == '.' or probe == '|':
-    matrix[y][x] = '|'
-    x, y = target
-    target = [x, y + 1]
-    x, y = target
-    probe = matrix[y][x]
-'''
-
 
 def print_matrix():
-    print()
-    global matrix, max_col, max_row
     for i in range(0, max_row):
         for j in range(494, max_col):
             print(matrix[i][j], end='', flush=True)
@@ -74,25 +41,15 @@ def check_empty(target, dv):
     dx, dy = dv
     x, y = target
     x, y = x + dx, y + dy
-    # print_matrix()
-    # print(matrix[y][x], matrix[y][x] == '.' or matrix[y][x] == '|', dv)
-   # if x >= max_col or y >= max_row:
-       # return False
-    
     return matrix[y][x] == '.' or matrix[y][x] == '|'
 
 
 def init_values():
     global right, left, down, matrix, max_col, max_row, data
-    right = [1, 0]
-    left = [-1, 0]
-    down = [0, 1]
-
+    right, left, down = [1, 0], [-1, 0], [0, 1]
     data = create_vector(initialization())
-    print(data)
     max_col = max(data, key=lambda x: x[0])[0] + 2
     max_row = max(data, key=lambda x: x[1])[1] + 4
-    print(max_col, max_row)
     matrix = [['.' for _ in range(0, max_col)] for _ in range(0, max_row)]
     for v in data:
         col, row = v
@@ -121,7 +78,6 @@ def step(target, dv):
 
 def fill():
     step([500, 0], [0, 0])
-    
     get_indexes = lambda x, xs: [i for (y, i) in zip(xs, range(len(xs))) if x == y]
     pool = list(filter(lambda x: x != [], [get_indexes('|', x) for x in matrix]))
     index = len(pool) - 1
@@ -131,146 +87,7 @@ def fill():
             matrix[index][x] = '~'
         fill()
         
-    print_matrix()
-    
-    #print(max(get_inde))
-    #if '|' not in matrix[max_row - 1]:
-        # break_row = False
-        # for i in range(0, max_row):
-        #     for j in range(0, max_col):
-        #         if matrix[max_row - i - 1][j] == '|':
-        #             matrix[max_row - i - 1][j] = '~'
-        #             break_row = True
-        #     if break_row:
-        #         break
-
-   # print_matrix()
-
-#except IndexError:
-        #print('calc som shit')
 
 init_values()
-# target = [500, 0]
-# step(target, [0, 0])
-# print_matrix()
-# break_row = False
-# for i in range(0, max_row):
-#     for j in range(494, max_col):
-#         if matrix[max_row - i - 1][j] == '|':
-#             matrix[max_row - i - 1][j] = '~'
-#             break_row = True
-#     if break_row:
-#         break
-# print_matrix()
-# target = [500, 0]
-# step(target, [0, 0])
-# print_matrix()
 fill()
 print_matrix()
-exit()
-
-
-print()
-break_row, found_platou = False
-for i in range(0, max_row):
-    for j in range(494, max_col):
-        if matrix[max_row - i - 1][j] == '#':
-            found_platou = True
-        if found_platou and matrix[max_row - i - 1][j] == '|':
-            matrix[max_row - i - 1][j] = '~'
-            break_row = True
-    if break_row:
-        break
-
-for i in range(0, max_row):
-    for j in range(494, max_col):
-        print(matrix[i][j], end='', flush=True)
-    print()
-print()
-
-
-def itterat(dv_arr):
-    dv = [0, 1]
-    # dx, dy = dv
-    while True:
-        x, y = target
-        matrix[y][x] = '|'
-        for dv in dv_arr:
-            dx, dy = dv
-            x, y = x + dx, y + dy
-            if matrix[y][x] != '.' and matrix[y][x] != '|':
-                break
-            target = [x, y]
-
-
-dv = [0, 1]
-dx, dy = dv
-while True:
-    x, y = target
-    matrix[y][x] = '|'
-    x, y = x + dx, y + dy
-    if matrix[y][x] != '.' and matrix[y][x] != '|':
-        break
-    target = [x, y]
-
-ends = 0
-dv = [1, 0]
-dx, dy = dv
-while True:
-    x, y = target
-    matrix[y][x] = '|'
-    x, y = x + dx, y + dy
-    if dy == 0 and ends == 0 and (matrix[y + 1][x] == '.' or matrix[y][x] == '|'):
-        if ends == 0:
-            target = [dx * -1, 0]
-        else:
-            target = [0, 1]
-        continue
-    
-    if matrix[y][x] != '.' and matrix[y][x] != '|':
-        ends += 1
-        break
-    target = [x, y]
-
-dv = [-1, 0]
-dx, dy = dv
-while True:
-    x, y = target
-    matrix[y][x] = '|'
-    x, y = x + dx, y + dy
-    if matrix[y][x] != '.' and matrix[y][x] != '|':
-        ends + 1
-        break
-    target = [x, y]
-
-print()
-break_row = False
-for i in range(0, max_row):
-    for j in range(494, max_col):
-        if matrix[max_row - i - 1][j] == '|':
-            matrix[max_row - i - 1][j] = '~'
-            break_row = True
-    if break_row:
-        break
-
-for i in range(0, max_row):
-    for j in range(494, max_col):
-        print(matrix[i][j], end='', flush=True)
-    print()
-print()
-
-'''
-Find +
-Check best path
-    if free down . or |
-        go down
-    else
-        go left and right
-        if left and right are blocked then go back with ~ until blocked (# or ~)
-        if left or right is blocked terminate the blocked
-        go to check best path -> continue until all threads are blocked
-        
-    
-down (. or |) --> if blocked (# or ~) --> two: left and right --> go until blocked (# or ~) -->
-if cross path: ~ --> until blocked (# or ~): terminate
-'''
