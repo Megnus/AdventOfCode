2f894aea370cd8739938b8a0f789b2b2fbb581a8
Magnus Sundström <megnus@hotmail.com>
2019-05-06 23:03:42 +0200

    Continued work with puzzle no 20.

--- Day_20.py
+++ Day_20.py
@@ -48,11 +48,15 @@ def explore(string, start, end_char):
 			x, y = checkpoint
 			checkpoint = (x + dx, y + dy)
 			path.append([char, checkpoint])
+			checkpoint = (x + 2*dx, y + 2*dy)
+			path.append([char, checkpoint])
 			print(char, end=' ')
 		elif char == '(':
 			# path.append(char)
 			index += explore(string[index:], checkpoint, ')')
 		elif char == '|':
+			#path.append(['R', checkpoint])
+
 			checkpoint = start
 			# path.append(char)
 			print('')
@@ -75,16 +79,54 @@ y_min = min([p[1] for p in koordinates])
 print(x_min, x_max, y_min, y_max)
 print(x_max - x_min, y_max - y_min)
 print('start: ', -x_min, -y_min)
-fields = [['*' for col in range(2 * (x_max - x_min))] for row in range(2 * (y_max - y_min))]
-start = [-x_min * 2, -y_min * 2]
+
+start = [-x_min, -y_min]
+koordinates = [start] + [[x[0] + start[0], x[1] + start[1]] for x in koordinates]
+for x in koordinates:
+	print(x)
+
+fields = [['#' for col in range(x_max - x_min + 1)] for row in range(y_max - y_min + 1)]
+print('-----------------')
+
+for v in koordinates:
+	col, row = v
+	fields[row][col] = '.'
+
+fields.reverse()
+for x in fields:
+	print(x)
+
+exit()
+
+
+
+
+
 koordinates = [[x[0] * 2 + start[0], x[1] * 2 + start[1]] for x in koordinates]
-koordinates = start + koordinates;
+koordinates = [start] + koordinates;
 print(koordinates)
+
+
 
 print(start)
 print(data)
 print(path)
 print(fields)
+print('-------')
+koord_ = [koordinates[0]]
+
+for x in zip(koordinates[:-1], koordinates[1:]):
+	koord_ += [[int((x[0][0] + x[1][0]) / 2), int((x[0][1] + x[1][1]) / 2)], x[1]]
+	fields[int((x[0][1] + x[1][1]) / 2)][int((x[0][0] + x[1][0]) / 2)] = '|'
+
+for x in koord_:
+	print(x)
+
+for x in koordinates:
+	fields[x[1]][x[0]] = ' '
+fields[6][4] = 'S'
+for x in fields:
+	print(x)
 
 """
 print("Result part 1: ", result_1)
