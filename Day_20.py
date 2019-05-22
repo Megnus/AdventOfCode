bb715c0e76965d3f46f221e2cdc721646b3a65a7
Magnus Sundström <megnus@hotmail.com>
2019-05-22 23:55:26 +0200

    Created path step counter.

--- Day_20.py
+++ Day_20.py
@@ -2,20 +2,20 @@ import re
 
 
 def initialization():
-	f = open('Input/input_day_20.txt', 'r')
-	in_data = f.read()
-	f.close()
-	print(in_data)
-	re_before = re.sub('\(.+?\)', '', in_data)
-	re_before = re.sub('^[A-Z]', '', 'EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)')
-	re_before = re.findall('[A-Z]+', in_data)
-	print(re_before)
-	re_before = re.findall('[\(|\)|\|]', in_data)
-	print(re_before)
-	re_before = re.findall('\(.+?\)', in_data)
-	print(re_before)
-	
-	"""
+    f = open('Input/input_day_20.txt', 'r')
+    in_data = f.read()
+    f.close()
+    print(in_data)
+    re_before = re.sub('\(.+?\)', '', in_data)
+    re_before = re.sub('^[A-Z]', '', 'EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)')
+    re_before = re.findall('[A-Z]+', in_data)
+    print(re_before)
+    re_before = re.findall('[\(|\)|\|]', in_data)
+    print(re_before)
+    re_before = re.findall('\(.+?\)', in_data)
+    print(re_before)
+
+    """
 	#re_before = re.findall('(?<=Before:\s\[).+(?=\])', in_data)
 	re_before = re.findall('(?<=\^).+(?=\$)', in_data)[0]
 	print(re_before)
@@ -26,8 +26,8 @@ def initialization():
 	re_before1 = re.findall('^.+?(?=[\(|\||\)])', article)
 	print(re_before1)
 	"""
-	# return re.findall('(?<=\^).+(?=\$)', in_data)[0]
-	return in_data
+    # return re.findall('(?<=\^).+(?=\$)', in_data)[0]
+    return in_data
 
 
 directions = {'E': (1, 0), 'W': (-1, 0), 'N': (0, 1), 'S': (0, -1)}
@@ -35,36 +35,36 @@ path = []
 
 
 def explore(string, start, end_char):
-	global path
-	checkpoint = start
-	print('')
-	index = 0
-	while index < len(string):
-		index += 1
-		char = string[index]
-		
-		if char in directions:
-			dx, dy = directions[char]
-			x, y = checkpoint
-			checkpoint = (x + dx, y + dy)
-			path.append([char, checkpoint])
-			checkpoint = (x + 2*dx, y + 2*dy)
-			path.append([char, checkpoint])
-			print(char, end=' ')
-		elif char == '(':
-			# path.append(char)
-			index += explore(string[index:], checkpoint, ')')
-		elif char == '|':
-			#path.append(['R', checkpoint])
+    global path
+    checkpoint = start
+    print('')
+    index = 0
+    while index < len(string):
+        index += 1
+        char = string[index]
 
-			checkpoint = start
-			# path.append(char)
-			print('')
-		elif char == end_char:
-			# path.append(char)
-			print('')
-			return index
-	return 0
+        if char in directions:
+            dx, dy = directions[char]
+            x, y = checkpoint
+            checkpoint = (x + dx, y + dy)
+            path.append([char, checkpoint])
+            checkpoint = (x + 2 * dx, y + 2 * dy)
+            path.append([char, checkpoint])
+            print(char, end=' ')
+        elif char == '(':
+            # path.append(char)
+            index += explore(string[index:], checkpoint, ')')
+        elif char == '|':
+            # path.append(['R', checkpoint])
+
+            checkpoint = start
+            # path.append(char)
+            print('')
+        elif char == end_char:
+            # path.append(char)
+            print('')
+            return index
+    return 0
 
 
 data = initialization()
@@ -85,50 +85,67 @@ koordinates = [start] + [[x[0] + start[0], x[1] + start[1]] for x in koordinates
 for x in koordinates:
     print(x)
 
-fields = [['#' for col in range(x_max - x_min + 1)] for row in range(y_max - y_min + 1)]
+fields = [[' # ' for col in range(x_max - x_min + 1)] for row in range(y_max - y_min + 1)]
 print('-----------------')
 
 for v in koordinates:
-	col, row = v
-	fields[row][col] = '.'
+    col, row = v
+    if [col, row] == start:
+        fields[row][col] = ' X '
+    else:
+        fields[row][col] = ' . '
 
+distances = [[0 for col in range(x_max - x_min + 1)] for row in range(y_max - y_min + 1)]
+# distances[-y_min][-x_min] = '1'
+
+
+for col, row in koordinates:
+    fields[row][col] = ' . '
+
+# fields[-y_min][-x_min] = 'X'
 fields.reverse()
+
 for x in fields:
-	print(x)
+    print(x)
 
-exit()
-
-
-
-
-
-koordinates = [[x[0] * 2 + start[0], x[1] * 2 + start[1]] for x in koordinates]
-koordinates = [start] + koordinates;
-print(koordinates)
-
-
-
-print(start)
-print(data)
-print(path)
-print(fields)
-print('-------')
-koord_ = [koordinates[0]]
-
-for x in zip(koordinates[:-1], koordinates[1:]):
-	koord_ += [[int((x[0][0] + x[1][0]) / 2), int((x[0][1] + x[1][1]) / 2)], x[1]]
-	fields[int((x[0][1] + x[1][1]) / 2)][int((x[0][0] + x[1][0]) / 2)] = '|'
-
-for x in koord_:
-	print(x)
-
-for x in koordinates:
-	fields[x[1]][x[0]] = ' '
-fields[6][4] = 'S'
-for x in fields:
-	print(x)
+print()
+ar = [(0, 1), (-1, 0), (1, 0), (0, -1)]
 
 """
-print("Result part 1: ", result_1)
-print("Result part 2: ", result_2)
+my = []
+for x in ar:
+    # ar.clear()
+    my.append(x)
+    print(x)
+print(my)
 """
+print()
+tracks = []
+tracks.append(start)
+col, row = start
+steps = 0
+fields[row][col] = f"{steps:03d}"
+
+
+while tracks:
+    print(tracks)
+    steps += 1
+    positions = []
+    for col, row in tracks:
+        for v in ar:
+            x, y = v
+            try:
+                if fields[row + y][col + x] == ' . ':
+                    fields[row + y][col + x] = f"{steps:03d}"
+                    positions.append([col + x, row + y])
+            except IndexError:
+                pass
+    #print(positions)
+
+    tracks = positions
+
+
+for x in fields:
+    print(x)
+
+print(f"{1:03d}")
