71f1f59ec6127d5524b86ca671d9cdb506735095
ARNA-00982-L <magnus.sundstrom@flir.se>
2019-06-20 16:00:41 +0200

    Working on second part for puzzle 22 with some small progress.

--- Day_22.py
+++ Day_22.py
@@ -2,10 +2,12 @@ import sys
 
 
 def initialization():
-	global depth, target
+	global depth, target, size
 	depth = 4080
 	target = (14, 785)
 	target = (10, 10)
+	x, y = target
+	size = x + int(x / 2) + 1, y + int(y / 2) + 1
 	depth = 510
 
 
@@ -46,67 +48,98 @@ def requierd_tool(type):
 
 
 def print_val():
-	for x in range(max_x):
-		for y in range(max_y):
+	for x in range(size_x):
+		for y in range(size_y):
 			score, _ = steps[y][x]
 			print(f"{score:03d}", end=' ')
 		print()
 
 
+def print_type(types):
+	t = ['.', '=', '|']
+	for x in range(size_x):
+		for y in range(size_y):
+			print(t[types[y][x]], end=' ')
+		print()
+	print()
+
 initialization()
+size_x, size_y = size
 target_x, target_y = target
 max_x, max_y = target_x + 1, target_y + 1
-erosions = [[0 for x in range(max_x)] for y in range(max_y)]
+erosions = [[0 for x in range(size_x)] for y in range(size_y)]
 
-for x in range(max_x):
-	for y in range(max_y):
+for x in range(size_x):
+	for y in range(size_y):
 		erosions[y][x] = erosion_level(geological_index((x, y), erosions))
 
 types = [[erosions[y][x] % 3 for x in range(max_x)] for y in range(max_y)]
 result_1 = sum([sum(x) for x in types]) - types[target_y][target_x]
 print("Result part 1: ", result_1)
 
-#sys.maxsize
-tools = [[set(requierd_tool(types[y][x])) for x in range(max_x)] for y in range(max_y)]
-steps = [[[999, set()] for x in range(max_x)] for y in range(max_y)]
-
-dv = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-x, y = (0, 0)
-steps[0][0] = [0, tools[0][0]]
 # walk = [0, tools[y][x]]
 # set1 = {0, 1}
 # set2 = {3, 2}
 # tup1 = set1.intersection(set2)
 
+area = [[erosions[y][x] % 3 for y in range(size_y)] for x in range(size_x)]
+print_type(area)
+
+# area = [[erosions[y][x] % 3 for x in range(size_x)] for y in range(size_y)]
+# sys.maxsize
+tools = [[set(requierd_tool(area[y][x])) for x in range(size_x)] for y in range(size_y)]
+
+t = ['.', '=', '|']
+for x in range(size_x):
+	for y in range(size_y):
+		print(t[area[y][x]], tools[y][x], end=' ')
+	print()
+print()
+steps = [[[999, set()] for y in range(size_y)] for x in range(size_x)]
+
+dv = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+x, y = (0, 0)
+steps[0][0] = [0, tools[0][0]]
+
 new_score = True
 while new_score:
 	new_score = False
-	for x in range(max_x):
-		for y in range(max_y):
+	for x in range(size_x - 1):
+		for y in range(size_y - 1):
+			# print(t[area[y][x]], tools[y][x], end=' ')
 			for dx, dy in dv:
-				print(x, y)
+				print(steps[y][x])
 				score, tool = steps[y][x]
 				px, py = x + dx, y + dy
-				print([px, py], [dx, dy])
-				if px >= max_x or py >= max_y or px < 0 or py < 0:
+				print(steps[py][px])
+				print(tool.intersection(tools[py][px]))
+				g = input();
+				
+				if px > size_x or py > size_y or px < 0 or py < 0:
 					continue
+				
 				new_tool = tools[py][px]
 				tool_in_hand = tool.intersection(new_tool)
-				print(tool_in_hand)
+				print("tool_in_hand", tool_in_hand, "[px, py]", [px, py], "tool", tool, "new_tool", new_tool)
+				g = input();
 				
 				if tool_in_hand:
 					score += 1
 				else:
 					score += 8
+					tool_in_hand = new_tool
 				
-				print(score, steps[py][px])
 				if score < steps[py][px][0]:
 					steps[py][px] = [score, tool_in_hand]
 					new_score = True
-				print_val()
-				print()
-				# g = input()
-					#break
+			
+				print(steps[py][px])
+				g = input();
+				
+		# print()
+
+print()
+print_val()
 
 
 
